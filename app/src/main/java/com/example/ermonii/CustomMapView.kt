17dd15e3da8569b5554f18fb41776a52dfcb56c4
MrAGC <alexclariana@gmail.com>
2025-02-17 20:14:26 +0100

    Animaciones de deslizar, deslizar con los dedos, y vista cuando pulsas evento

--- app/src/main/java/com/example/ermonii/CustomMapView.kt
+++ app/src/main/java/com/example/ermonii/CustomMapView.kt
@@ -1,21 +1,21 @@
-package com.example.ermonii
+    package com.example.ermonii
 
-import android.content.Context
-import android.util.AttributeSet
-import org.osmdroid.views.MapView
+    import android.content.Context
+    import android.util.AttributeSet
+    import org.osmdroid.views.MapView
 
-class CustomMapView : MapView {
+    class CustomMapView : MapView {
 
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+        constructor(context: Context) : super(context)
+        constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 
-    var isInteracting = false
+        var isInteracting = false
 
-    override fun onTouchEvent(event: android.view.MotionEvent): Boolean {
-        when (event.action) {
-            android.view.MotionEvent.ACTION_DOWN -> isInteracting = true
-            android.view.MotionEvent.ACTION_UP, android.view.MotionEvent.ACTION_CANCEL -> isInteracting = false
+        override fun onTouchEvent(event: android.view.MotionEvent): Boolean {
+            when (event.action) {
+                android.view.MotionEvent.ACTION_DOWN -> isInteracting = true
+                android.view.MotionEvent.ACTION_UP, android.view.MotionEvent.ACTION_CANCEL -> isInteracting = false
+            }
+            return super.onTouchEvent(event)
         }
-        return super.onTouchEvent(event)
     }
-}
