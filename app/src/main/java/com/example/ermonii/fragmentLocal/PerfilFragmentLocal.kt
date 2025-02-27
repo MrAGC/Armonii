847d92034d0e5e1e4625ded71e2deea381f5c0a6
DevRoger <ralonsop2324@politecnics.barcelona>
2025-02-27 18:53:54 +0100

    Implementado dialog_edit_local.xml y dialog_edit_musico.xml para editar los perfiles de cada usuario.

--- app/src/main/java/com/example/ermonii/fragmentLocal/PerfilFragmentLocal.kt
+++ app/src/main/java/com/example/ermonii/fragmentLocal/PerfilFragmentLocal.kt
@@ -1,17 +1,22 @@
 package com.example.ermonii.fragmentLocal
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.ermonii.IniciarSesion
 import com.example.ermonii.R
 
 class PerfilFragmentLocal : Fragment() {
     private lateinit var btnCerrarSesion: Button
+    private lateinit var btn_editarPerfil: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -28,6 +33,36 @@ class PerfilFragmentLocal : Fragment() {
             requireActivity().finish()
         }
 
+        btn_editarPerfil = view.findViewById(R.id.btn_editarPerfil)
+
+        btn_editarPerfil.setOnClickListener {
+            showEditDialog(requireContext())
+        }
+
         return view
+    }
+
+    @SuppressLint("MissingInflatedId")
+    private fun showEditDialog(context: Context) {
+        // Infla el diseño del diálogo
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_edit_local, null)
+
+        // Inicializa los EditText y el botón del diálogo
+        val btnCancelar = dialogView.findViewById<Button>(R.id.btnCancelar)
+
+        // Crea el AlertDialog
+        val dialogBuilder = AlertDialog.Builder(context)
+            .setView(dialogView)
+            .setCancelable(true)
+
+        // Crea el AlertDialog
+        val dialog = dialogBuilder.create()
+
+        btnCancelar.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        // Muestra el diálogo
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/ermonii/fragmentMusico/PerfilFragmentMusico.kt
+++ app/src/main/java/com/example/ermonii/fragmentMusico/PerfilFragmentMusico.kt
@@ -1,5 +1,7 @@
 package com.example.ermonii.fragmentMusico
 
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,12 +9,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import com.example.ermonii.IniciarSesion
 import com.example.ermonii.R
 
 class PerfilFragmentMusico : Fragment() {
 
     private lateinit var btnCerrarSesion: Button
+    private lateinit var btn_editarPerfil: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +35,38 @@ class PerfilFragmentMusico : Fragment() {
             requireActivity().finish()
         }
 
+        btn_editarPerfil = view.findViewById(R.id.btn_editarPerfil)
+
+        btn_editarPerfil.setOnClickListener {
+            showEditDialog(requireContext())
+        }
+
         return view
     }
+
+    private fun showEditDialog(context: Context) {
+        // Infla el diseño del diálogo
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_edit_musico, null)
+
+        // Inicializa los EditText y el botón del diálogo
+        val edtNombre = dialogView.findViewById<EditText>(R.id.edtNombre)
+        val edtCorreo = dialogView.findViewById<EditText>(R.id.edtCorreo)
+        val btnCancelar = dialogView.findViewById<Button>(R.id.btnCancelar)
+
+        // Crea el AlertDialog
+        val dialogBuilder = AlertDialog.Builder(context)
+            .setView(dialogView)
+            .setCancelable(true)
+
+        // Crea el AlertDialog
+        val dialog = dialogBuilder.create()
+
+        btnCancelar.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        // Muestra el diálogo
+        dialog.show()
+    }
+
 }
