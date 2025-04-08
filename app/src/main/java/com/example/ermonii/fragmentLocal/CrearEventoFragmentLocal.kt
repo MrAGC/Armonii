755aa5f1b1634b2dcb0e2ab0ebff6cd04cda381e
DevRoger <ralonsop2324@politecnics.barcelona>
2025-04-08 18:47:30 +0200

    Implementadas restricciones a la hora de crear Eventos

--- app/src/main/java/com/example/ermonii/fragmentLocal/CrearEventoFragmentLocal.kt
+++ app/src/main/java/com/example/ermonii/fragmentLocal/CrearEventoFragmentLocal.kt
@@ -87,50 +87,122 @@ class CrearEventoFragmentLocal : Fragment() {
 
         // Progragmatica para crear evento
         btnCrearEvento.setOnClickListener {
-            val fechaString = edtFecha.text.toString()
-            val horaString = edtHora.text.toString()
+            if (edtNombre.text.toString() == "") {
+                edtNombre.setBackgroundResource(R.drawable.redondear_edittext_error)
+                edtFecha.setBackgroundResource(R.drawable.redondear_edittext)
+                edtHora.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDescripcion.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDuracion.setBackgroundResource(R.drawable.redondear_edittext)
 
-            // Concatenamos la fecha y hora en el formato "yyyy-MM-ddTHH:mm:ss"
-            val fechaHoraFinal = fechaString + "T" + horaString + ":00"
+                Toast.makeText(requireContext(), "Inserta un nombre válido", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (edtFecha.text.toString() == "") {
+                edtNombre.setBackgroundResource(R.drawable.redondear_edittext)
+                edtFecha.setBackgroundResource(R.drawable.redondear_edittext_error)
+                edtHora.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDescripcion.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDuracion.setBackgroundResource(R.drawable.redondear_edittext)
 
-            // HAY QUE CORREGIR EL ID DE LOCAL
-            val eventoNuevo = Evento(0, edtNombre.text.toString(),fechaHoraFinal,edtDescripcion.text.toString(), 1, null, true, edtDuracion.text.toString().toInt())
+                Toast.makeText(requireContext(), "Inserta una fecha válida", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (edtHora.text.toString() == "") {
+                edtNombre.setBackgroundResource(R.drawable.redondear_edittext)
+                edtFecha.setBackgroundResource(R.drawable.redondear_edittext)
+                edtHora.setBackgroundResource(R.drawable.redondear_edittext_error)
+                edtDescripcion.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDuracion.setBackgroundResource(R.drawable.redondear_edittext)
 
-            Log.d("API_EventoNuevo", """
-                ===== EVENTO =====
-                ID: ${eventoNuevo.id}
-                Nombre: ${eventoNuevo.nombre}
-                Local: ${eventoNuevo.idLocal}
-                Musico: ${eventoNuevo.idMusico}
-                Descripcion: ${eventoNuevo.descripcion}
-                Fecha: ${eventoNuevo.fecha}
-                Duracion: ${eventoNuevo.duracion}                
-                """.trimIndent())
+                Toast.makeText(requireContext(), "Inserta una hora válida", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (edtDescripcion.text.toString() == "") {
+                edtNombre.setBackgroundResource(R.drawable.redondear_edittext)
+                edtFecha.setBackgroundResource(R.drawable.redondear_edittext)
+                edtHora.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDescripcion.setBackgroundResource(R.drawable.redondear_edittext_error)
+                edtDuracion.setBackgroundResource(R.drawable.redondear_edittext)
 
-            RetrofitClient.instance.postEvento(eventoNuevo).enqueue(object : Callback<Boolean> {
-                override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
-                    if (response.isSuccessful) {
-                        Toast.makeText(
-                            requireContext(),"Creado correctamente!",
-                            Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Inserta una fecha válida", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (edtDuracion.text.toString() == "") {
+                edtNombre.setBackgroundResource(R.drawable.redondear_edittext)
+                edtFecha.setBackgroundResource(R.drawable.redondear_edittext)
+                edtHora.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDescripcion.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDuracion.setBackgroundResource(R.drawable.redondear_edittext_error)
 
-                        // Reseteamos los campos
-                        edtNombre.setText("")
-                        edtFecha.setText("")
-                        edtHora.setText("")
-                        edtDescripcion.setText("")
-                        edtDuracion.setText("")
+                Toast.makeText(requireContext(), "Inserta una fecha válida", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                edtNombre.setBackgroundResource(R.drawable.redondear_edittext)
+                edtFecha.setBackgroundResource(R.drawable.redondear_edittext)
+                edtHora.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDescripcion.setBackgroundResource(R.drawable.redondear_edittext)
+                edtDuracion.setBackgroundResource(R.drawable.redondear_edittext)
 
-                            Log.d("API_RESPONSE", "Evento registrado correctamente: ${response.body()}")
-                    } else {
-                        Log.e("API_ERROR", "Error al registrar el evento: ${response.errorBody()?.string()}")
+
+                val fechaString = edtFecha.text.toString()
+                val horaString = edtHora.text.toString()
+
+                // Concatenamos la fecha y hora en el formato "yyyy-MM-ddTHH:mm:ss"
+                val fechaHoraFinal = fechaString + "T" + horaString + ":00"
+
+                // HAY QUE CORREGIR EL ID DE LOCAL
+                val eventoNuevo = Evento(
+                    0,
+                    edtNombre.text.toString().trim(),
+                    fechaHoraFinal,
+                    edtDescripcion.text.toString().trim(),
+                    1,
+                    null,
+                    true,
+                    edtDuracion.text.toString().toInt()
+                                        )
+
+                Log.d(
+                    "API_EventoNuevo", """
+                    ===== EVENTO =====
+                    ID: ${eventoNuevo.id}
+                    Nombre: ${eventoNuevo.nombre}
+                    Local: ${eventoNuevo.idLocal}
+                    Musico: ${eventoNuevo.idMusico}
+                    Descripcion: ${eventoNuevo.descripcion}
+                    Fecha: ${eventoNuevo.fecha}
+                    Duracion: ${eventoNuevo.duracion}                
+                    """.trimIndent()
+                     )
+
+
+                RetrofitClient.instance.postEvento(eventoNuevo).enqueue(object : Callback<Boolean> {
+                    override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
+                        if (response.isSuccessful) {
+                            Toast.makeText(
+                                requireContext(), "Creado correctamente!", Toast.LENGTH_SHORT
+                                          ).show()
+
+                            // Reseteamos los campos
+                            edtNombre.setText("")
+                            edtFecha.setText("")
+                            edtHora.setText("")
+                            edtDescripcion.setText("")
+                            edtDuracion.setText("")
+
+                            Log.d(
+                                "API_RESPONSE",
+                                "Evento registrado correctamente: ${response.body()}"
+                                 )
+                        } else {
+                            Log.e(
+                                "API_ERROR",
+                                "Error al registrar el evento: ${response.errorBody()?.string()}"
+                                 )
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<Boolean>, t: Throwable) {
-                    Log.e("API_FAILURE", "Fallo en la conexión: ${t.message}")
-                }
-            })
+                    override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                        Log.e("API_FAILURE", "Fallo en la conexión: ${t.message}")
+                    }
+                })
+            }
         }
 
 
@@ -139,15 +211,6 @@ class CrearEventoFragmentLocal : Fragment() {
 
         return view
     }
-
-    fun setNumberPickerTextSize(picker: NumberPicker, sizeInSp: Float) {
-        val count = picker.childCount
-        for (i in 0 until count) {
-            val child = picker.getChildAt(i)
-            if (child is EditText) {
-                child.textSize = sizeInSp
-                break
-            }
-        }
     }
-}
+
+// .setBackgroundResource(R.drawable.redondear_edittext_error)
